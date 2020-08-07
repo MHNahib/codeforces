52f614463802d41cec03e507090fd11709b9b5ed
MHNahib <mhnahib@gmail.com>
2020-08-07 10:51:33 +0600

    Solved : A. Watermelon

---
+++ brute force - math/4A-Watermelon.c
@@ -0,0 +1,16 @@
+/*
+    A. Watermelon
+    Status: Solved
+    Link: https://codeforces.com/contest/4/problem/A
+*/
+#include<stdio.h>
+int main()
+{
+    int w;
+    scanf("%d",&w);
+    if(w%2!=0||w<=2)
+        printf("NO\n");
+    else
+        printf("YES\n");
+    return 0;
+}
